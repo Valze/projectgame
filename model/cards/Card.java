a4f8043c26e8657709a289612c97c7dbda2cf044
Valz <ahmadahussein0@gmail.com>
2020-03-01 16:17:20 +0200

    Initialized abstract Card class with appropriate methods, access modifiers, and Rarity Enum

--- model/cards/Card.java
+++ model/cards/Card.java
@@ -1,5 +1,27 @@
 package model.cards;
 
 public abstract class Card {
-
+	private String name;
+	private int manaCost;
+	private Rarity rarity;
+	public Card() {
+		
+	}
+	public Card(String name, int manaCost, Rarity rarity) {
+		this.name = name;
+		this.manaCost = manaCost;
+		this.rarity = rarity;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public int getManaCost() {
+		return this.manaCost;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setManaCost(int manaCost) {
+		this.manaCost = manaCost;
+	}
 }
