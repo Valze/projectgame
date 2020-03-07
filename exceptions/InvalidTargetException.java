8ad721e716eeff012353d032c2136444db45eab8
Valz <ahmadahussein0@gmail.com>
2020-03-07 19:10:51 +0200

    Remove custom message in empty InvalidTargetException constructor
    We shouldn't raise a custom message with the default constructor

--- exceptions/InvalidTargetException.java
+++ exceptions/InvalidTargetException.java
@@ -3,7 +3,7 @@ package exceptions;
 public class InvalidTargetException extends HearthstoneException {
 
 	public InvalidTargetException() {
-		super("Icehowl cannot attack hero.");
+		super();
 	}
 
 	public InvalidTargetException(String s) {
