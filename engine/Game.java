42a6a7967282fa6fa14c440272e7316fa0504b8c
Omar <omarelnabulsi2000@gmail.com>
2020-04-07 18:16:31 +0200

    Updated Some of Validators

--- engine/Game.java
+++ engine/Game.java
@@ -3,6 +3,7 @@ package engine;
 import exceptions.CannotAttackException;
 import exceptions.FullFieldException;
 import exceptions.FullHandException;
+import exceptions.HeroPowerAlreadyUsedException;
 import exceptions.InvalidTargetException;
 import exceptions.NotEnoughManaException;
 import exceptions.NotSummonedException;
@@ -11,7 +12,7 @@ import exceptions.TauntBypassException;
 import model.cards.Card;
 import model.cards.minions.Minion;
 import model.heroes.*;
- 
+
 public class Game implements ActionValidator {
 
 	private Hero firstHero;
@@ -47,22 +48,49 @@ public class Game implements ActionValidator {
 	}
 	
 	public void validateTurn(Hero user) throws NotYourTurnException{
-		
+		if(user != currentHero) {
+			throw new NotYourTurnException("This is not your turn");
+		}
 	}
 	public void validateAttack(Minion attacker,Minion target) throws CannotAttackException,NotSummonedException,TauntBypassException,InvalidTargetException{
-		
+		if(attacker.isSleeping() == true) {
+			throw new CannotAttackException("Minion is Sleeping");
+		}
+		else if(!currentHero.getField().contains(attacker)) {
+			throw new NotSummonedException("Minion is in hand");
+		}
+		else if(currentHero.getField().contains(target)) {
+			throw new InvalidTargetException("Cannot attack friendly minions");
+		}
+		else if(attacker.getAttack()<=0) {
+			throw new CannotAttackException("Minion has no attack power");
+		}
+		else if(!opponent.getField().contains(target)) {
+			throw new NotSummonedException("Target not summoned");
+		}
 	}
 	public void validateAttack(Minion attacker,Hero target) throws CannotAttackException,NotSummonedException,TauntBypassException,InvalidTargetException{
-		
+		if(attacker.isSleeping() == true) {
+			throw new CannotAttackException("Minion is Sleeping");
+		}
+		else if(!currentHero.getField().contains(attacker)) {
+			throw new NotSummonedException("Minion is in hand");
+		}
 	}
 	public void validateManaCost(Card card) throws NotEnoughManaException{
-		
+		if(card.getManaCost()> currentHero.getCurrentManaCrystals()) {
+			throw new NotEnoughManaException("Not enough mana");
+		}
 	}
 	public void validatePlayingMinion(Minion minion) throws FullFieldException{
-		
+		if(currentHero.getField().size()>=7) {
+			throw new FullFieldException("Field is full");
+		}
 	}
-	public void validateUsingHeroPower(Hero hero) throws NotEnoughManaException{
-		
+	public void validateUsingHeroPower(Hero hero) throws NotEnoughManaException, HeroPowerAlreadyUsedException{
+		if(hero.isHeroPowerUsed() == true) {
+			throw new HeroPowerAlreadyUsedException("Hero power already used");
+		}
 	}
 	public void damageOpponent(int amount) {
 		
