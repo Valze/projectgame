f9323d159d40d7343fbb2c6624b3c8df55eceb5a
Valz <ahmadahussein0@gmail.com>
2020-04-10 13:45:35 +0200

    Changed MultiShot's death handling as setCurrentHP handles minion death if it falls below 0

--- model/cards/spells/MultiShot.java
+++ model/cards/spells/MultiShot.java
@@ -19,29 +19,24 @@ public class MultiShot extends Spell implements AOESpell {
 		case 1: if(oppField.get(0).isDivine()) oppField.get(0).setDivine(false);
 				else {
 					oppField.get(0).setCurrentHP(oppField.get(0).getCurrentHP() - 3);
-					if(oppField.get(0).getCurrentHP() <= 0) oppField.remove(0).minionDeath();
 				}
 				break;
 		case 2: if(oppField.get(0).isDivine()) oppField.get(0).setDivine(false);
 				else {
 					oppField.get(0).setCurrentHP(oppField.get(0).getCurrentHP() - 3);
-					if(oppField.get(0).getCurrentHP() <= 0) oppField.remove(0).minionDeath();
 				}
 				if(oppField.get(1).isDivine()) oppField.get(1).setDivine(false);
 				else {
 					oppField.get(1).setCurrentHP(oppField.get(1).getCurrentHP() - 3);
-					if(oppField.get(1).getCurrentHP() <= 0) oppField.remove(1).minionDeath();
 				}
 				break;
 		default:	if(oppField.get(oppField.size()/2).isDivine()) oppField.get(oppField.size()/2).setDivine(false);
 					else {
 						oppField.get(oppField.size()/2).setCurrentHP(oppField.get(oppField.size()/2).getCurrentHP() - 3);
-						if(oppField.get(oppField.size()/2).getCurrentHP() <= 0) oppField.remove(oppField.size()/2).minionDeath();
 					}
 					if(oppField.get((oppField.size()/2)-1).isDivine()) oppField.get((oppField.size()/2)-1).setDivine(false);
 					else {
 						oppField.get((oppField.size()/2)-1).setCurrentHP(oppField.get((oppField.size()/2)-1).getCurrentHP() - 3);
-						if(oppField.get((oppField.size()/2)-1).getCurrentHP() <= 0) oppField.remove((oppField.size()/2)-1).minionDeath();
 					}
 		}
 	}
