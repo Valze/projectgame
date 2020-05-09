25daef72165ea1018db0ad0f0368f3393e6da49f
Ahmad Abdallah <ahmadahussein0@gmail.com>
2020-05-09 21:25:48 +0300

    Added field, allowed minions to do attacks, and end turn

--- view/start/HeroPanel.java
+++ view/start/HeroPanel.java
@@ -1,36 +1,64 @@
 package view.start;
 import javax.swing.*;
 
+import model.cards.Card;
+import model.cards.Rarity;
+import model.cards.minions.Minion;
+import model.cards.spells.Spell;
 import model.heroes.Hero;
 
 import java.awt.*;
 public class HeroPanel extends JPanel{
 	public class CardList extends JList{
-		public CardList(Object[] cards) {
+		public int size;
+		public CardList concealed;
+		public CardList(Object[] cards, int size) {
 			super(cards);
+			this.size = size;
+			this.concealed = this.hideCards();
 			this.setVisibleRowCount(1);
 			this.setLayoutOrientation(JList.HORIZONTAL_WRAP);
 		}
-		public CardList update(Object[] cards) {
-			CardList updated = new CardList(cards);
+		public CardList updateHand(Object[] cards) {
+			CardList updated = new CardList(cards, cards.length);
 			return updated;
 		}
-		public void play(){
-			
+		public CardList hideCards(){
+			if(size==0) {
+				return null;
+			}
+			Object[] hidden = new Object[size];
+			for(int i = 0; i<size; i++) {
+				Card hide = new Minion("HIDDEN", 0, Rarity.BASIC, 0, 0, false, false, false);
+				hidden[i] = hide;
+			}
+			CardList empty = new CardList(hidden, 0);
+			return empty;
 		}
 	}
 	Hero related;
 	JLabel heroName;
 	JButton heroPower;
 	CardList cards;
+	JPanel cardHolder; //using a panel to center cards
+	public void hideCards() {
+		cardHolder.remove(cards);
+		cardHolder.add(cards.concealed);
+	}
+	public void update() {
+		
+	}
 	public HeroPanel(Hero h, boolean Up) {
 		super(new BorderLayout());
+		this.cardHolder = new JPanel();
 		related = h;
 		String heroInfo = "<div style='text-align:center;'>" + "<h1>"+h.getName() + "</h1>"
 						+ "<br>HP: " + h.getCurrentHP() 
-						+"<br>Mana: "+ h.getCurrentManaCrystals() +" out of " +h.getTotalManaCrystals()+"</div>";
+						+"<br>Mana: "+ h.getCurrentManaCrystals() +" out of " +h.getTotalManaCrystals()
+						+"<br>Cards in deck: "+h.getDeck().size()+"</div>";
 		heroName = new JLabel("<html> "+ heroInfo + "</html>");
-		cards = new CardList(h.getHand().toArray());
+		cards = new CardList(h.getHand().toArray(), h.getHand().size());
+		cardHolder.add(cards);
 		heroPower = new JButton("<html> Use Hero Power </html>");
 		JPanel internal = new JPanel(); //JPanel to center Hero information
 		internal.add(heroName);
@@ -39,15 +67,15 @@ public class HeroPanel extends JPanel{
         box.add(Box.createVerticalGlue());
         box.add(internal);     
         box.add(Box.createVerticalGlue());
-		//endTurn.addActionListener();
 		this.add(BorderLayout.CENTER, box);
 		if(Up) { //if the hero is supposed to be located in the northern
 			//part of the screen, place his cards above him
 			//otherwise place them below him
-			this.add(BorderLayout.NORTH, cards);
+			this.add(BorderLayout.NORTH, cardHolder);
+			this.hideCards();
 		}
 		else {
-			this.add(BorderLayout.SOUTH, cards);
+			this.add(BorderLayout.SOUTH, cardHolder);
 		}
 		//this.setSize(new Dimension(50,50));
 		//this.setMaximumSize(new Dimension(100, 100));
--- view/start/Main.java
+++ view/start/Main.java
@@ -1,33 +1,28 @@
 package view.start;
 
 import java.awt.*;
+
 import java.io.IOException;
 
 import javax.swing.*;
 import engine.Game;
 import exceptions.FullHandException;
+import model.cards.Rarity;
+import model.cards.minions.Minion;
+import model.heroes.Hero;
 import model.heroes.Hunter;
 import model.heroes.Paladin;
-import model.heroes.Hero;
+import model.heroes.Priest;
+import model.heroes.Warlock;
+import model.heroes.Mage;
 public class Main {
 	public static void main(String[] args) throws IOException, CloneNotSupportedException, FullHandException {
-		JFrame window = new JFrame();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setSize(600, 600);
-		window.setLayout(new BorderLayout());
-		Hero hunter = new Hunter();
+		Hero hunter = new Paladin();
 		Hero paladin = new Paladin();
-		Game game = new Game(hunter, paladin);
-		HeroPanel firstHero = new HeroPanel(hunter, false);
-		JButton endTurn = new JButton("End turn");
-		//HeroPanels are responsible for rendering each Hero's
-		//HP, mana, hero power, and hand
-		HeroPanel secondHero = new HeroPanel(paladin, true);
-		window.add(BorderLayout.SOUTH, firstHero);
-		window.add(BorderLayout.EAST, endTurn);
-		JTextArea text = new JTextArea();
-		window.add(BorderLayout.CENTER, text);
-		window.add(BorderLayout.NORTH, secondHero);
-		window.setVisible(true);
+		//TODO: replace hunter,paladin with heroes from a selection screen
+		//How?
+		//Subclass JFrame like in GameWindow, then create its specific GUI components for
+		//hero selection, then return selected heroes here and pass them to GameWindow
+		GameWindow main = new GameWindow(hunter, paladin);
 	}
 }
